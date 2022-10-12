6bb0190eea02c58ca2d6d81763dbecade6fafc6f
loekandy <loekandy@gmail.com>
2022-10-12 17:01:00 +0200

    go

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,12 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "bootstrap/dist/css/bootstrap.css";
 import "../scss/main.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Single Shared Layout with Custom App
 // https://nextjs.org/docs/basic-features/layouts
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,7 +17,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
       />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </Layout>
   );
 }
--- pages/weather/index.tsx
+++ pages/weather/index.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import Draggable from "react-draggable"; // Both at the same time
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
+// https://andyland-api.azurewebsites.net/api/Weather/weatherLocationsList
 const WeatherMap = () => {
+  const { data, isSuccess } = useQuery(["joke"], async () => {
+    const { data } = await axios(
+      "https://andyland-api.azurewebsites.net/api/Weather/weatherLocationsList"
+    );
+    return data;
+  });
+
+  console.log(data);
+
   return (
     <div>
       <>
+        <div className="position-absolute">
+          <Draggable>
+            <div className="d-block bg-white text-dark rounded p-2 fw-bolder shadow">
+              {data && data[1].temperature} °C
+            </div>
+          </Draggable>
+          <Draggable>
+            <div className=" bg-info text-dark rounded p-0 d-flex justify-content-center fw-bolder shadow">
+              {data && data[1].windSpeed} m/s
+            </div>
+          </Draggable>
+        </div>
         <section className="d-flex position-absolute flex-column text-dark text-center mt-3 ms-2 d-none">
           <span className="bg-dark text-white text-center p-1">Något!</span>
           <div className="shadow flex-row m-2 rounded justify-content-center">
@@ -17,7 +42,7 @@ const WeatherMap = () => {
           src="https://earth.nullschool.net/#current/wind/surface/level/orthographic=-344.73,62.00,2845/grid=on/"
           height={"1000px"}
           width={"100%"}
-        ></iframe>
+        />
       </>
     </div>
   );
